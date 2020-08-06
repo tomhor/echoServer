45d25cad4eb1e678a7878fd1d063e24b23ed1482
tomhor <tomhorvitz@gmail.com>
2020-08-06 10:17:29 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+app.get('/', function (req, res){
+    res.send(true);
+});
+
+
+httpServer.listen(8080, () => console.log('listening to port 3001!'));
+httpsServer.listen(8443);
